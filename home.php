f252eb86064e4c4fcf03d6f51b181171c50ddbfe
Labesh <labheshwar.work@gmail.com>
2024-01-19 16:03:18 +0500

    Blogs styling

--- home.php
+++ home.php
@@ -2,33 +2,36 @@
 require_once 'utilities/blogposts.php';
 require_once 'utilities/user.php';
 
-	if (!is_user_loggedin()) {
-		header("Location: index.php");
-		return;
-	}
+if (!is_user_loggedin()) {
+	header("Location: index.php");
+	return;
+}
 
-	$blogposts = get_all_posts();
+$blogposts = get_all_posts();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<link rel="stylesheet" href="style.css">
-		<title>Home</title>
-	</head>
-	<body>
-		
-		<?php include "header.php";?>
-		<?php if(isset($_SESSION["flash_message"])):?>
-			<div class="success-message"><?=$_SESSION["flash_message"];?></div>
-			<?php unset($_SESSION["flash_message"]);
-			 endif;
-			?>
-		<div style="text-align: center">
-			<h1>This is home page</h1>
+
+<head>
+	<meta charset="UTF-8" />
+	<link rel="stylesheet" href="style.css">
+	<title>Home</title>
+</head>
+
+<body>
+
+	<?php include "header.php"; ?>
+	<?php if (isset($_SESSION["flash_message"])) : ?>
+		<div class="success-message"><?= $_SESSION["flash_message"]; ?></div>
+	<?php unset($_SESSION["flash_message"]);
+	endif;
+	?>
+	<div style="text-align: center">
+		<h1>Latest blogs</h1>
+		<div class="blog-wrapper">
 			<?php
-			if ($blogposts != null):
-				foreach($blogposts as $blogpost):
+			if ($blogposts != null) :
+				foreach ($blogposts as $blogpost) :
 					$author = $blogpost["user_full_name"];
 					$post_id = $blogpost["post_id"];
 					$post_title = $blogpost["post_title"];
@@ -37,37 +40,39 @@ require_once 'utilities/user.php';
 					$reads = $blogpost["_reads"];
 					$post_date = $blogpost["post_date"]; // String object
 					$post_date = date_create($post_date); // DateTime object
-					$post_date = date_format($post_date,"jS, F, Y.");
+					$post_date = date_format($post_date, "jS, F, Y.");
 			?>
-			<section class="blogpost">
-				<div class="blogtitle"><?=$post_title?></div>
-				<div class="blogauthor">By <?=$author?></div>
-				<div><?=$post_body?>. <a href="post.php?id=<?=$post_id?>">Read more...</a></div>
-				<div class="blogpostfooter">
-					<!-- Note: Never expose database ids in urls -->
-					<?php if($likes > 0): ?>
-					<a href="postlikes.php?id=<?=$post_id?>">
-					<?php endif;?>
-					<span class="blogdate"><small><i class="count"><?=$likes?></i> people like the blog.</small></span>
-					<?php if($likes > 0): ?>
-					</a>
-					<?php endif;?>
+					<section class="blogpost">
+						<div class="blogtitle"><?= $post_title ?></div>
+						<div class="blogauthor">By <?= $author ?></div>
+						<div><?= $post_body ?>. <a style="color: white; text-decoration: underline; font-size: 12px;" href="post.php?id=<?= $post_id ?>">read more...</a></div>
+						<div class="blogpostfooter">
+							<!-- Note: Never expose database ids in urls -->
+							<?php if ($likes > 0) : ?>
+								<a href="postlikes.php?id=<?= $post_id ?>">
+								<?php endif; ?>
+								<span class="blogdate"><small><i class="count"><?= $likes ?></i> people like the blog.</small></span>
+								<?php if ($likes > 0) : ?>
+								</a>
+							<?php endif; ?>
 
-					<?php if($reads > 0): ?>
-					<a href="postreads.php?id=<?=$post_id?>">
-					<?php endif;?>
-					<span class="blogdate"><small><i class="count"><?=$reads?></i> people have read the blog.</small></span>
-					<?php if($reads > 0): ?>
-					</a>
-					<?php endif;?>
-				
-					<div class="blogdate"><small>Posted on: <?=$post_date?></small></div>
-				</div>
-			</section>
+							<?php if ($reads > 0) : ?>
+								<a href="postreads.php?id=<?= $post_id ?>">
+								<?php endif; ?>
+								<span class="blogdate"><small><i class="count"><?= $reads ?></i> people have read the blog.</small></span>
+								<?php if ($reads > 0) : ?>
+								</a>
+							<?php endif; ?>
+
+							<div class="blogdate"><small>Posted on: <?= $post_date ?></small></div>
+						</div>
+					</section>
 			<?php
-			endforeach;
+				endforeach;
 			endif;
 			?>
 		</div>
-	</body>
+	</div>
+</body>
+
 </html>
