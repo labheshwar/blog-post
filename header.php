fa294fcd8f466d473ad41a41006d6f976f4bb716
Labesh <labheshwar.work@gmail.com>
2024-01-19 15:46:42 +0500

    Header styling

--- header.php
+++ header.php
@@ -1,20 +1,17 @@
 <?php
-
 $user = isset($_SESSION['_user']) ? $_SESSION['_user'] : null;
-if(isset($user)) {
+if (isset($user)) {
     $login_menu_item = "Hello " . $user["user_full_name"] . ' (<a href="logout.php">Logout</a>)';
 }
-
 ?>
+
 <header>
-    <nav>
-        <ul style="column-count: 4; list-style: none">
-            <li><a href="home.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <?php if(isset($login_menu_item)):?>
-                <li><a href="addblog.php">Add blog</a></li>
-                <li><?=$login_menu_item?></li>
-            <?php endif;?>
-        </ul>
-    </nav>
+    <ul class="header-menu">
+        <li><a href="home.php" class="menu-item">Home</a></li>
+        <li><a href="about.php" class="menu-item">About</a></li>
+        <?php if (isset($login_menu_item)) : ?>
+            <li><a href="addblog.php" class="menu-item">Add Blog</a></li>
+            <li class="login-menu-item"><?= $login_menu_item ?></li>
+        <?php endif; ?>
+    </ul>
 </header>
